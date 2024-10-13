d546bacd58e564dcf300b7d356be65361c3fc19d
Arman11r <arman0078627@gmail.com>
2024-10-13 21:21:10 +0530

    Search functionality added
    search bar changes

--- Script.js
+++ Script.js
@@ -1,20 +1,29 @@
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
-
     const foodImages = document.querySelectorAll(".Recipe");
+    const searchInput = document.getElementById('search');
 
     foodImages.forEach(image => {
         image.addEventListener("click", () => {
             const recipePath = image.getAttribute("recipe-page");
             if (recipePath) {
-                window.location.href =recipePath;
+                window.location.href = recipePath;
             } else {
-                console.error("Recipe path not found for this .");
+                console.error("Recipe path not found for this.");
             }
         });
-
         image.style.cursor = "pointer";
+    });
+    searchInput.addEventListener('input', function() {
+        const filter = searchInput.value.toLowerCase();
+
+
+        foodImages.forEach(recipe => {
+            const recipeName = recipe.querySelector('h3').textContent.toLowerCase();
+            if (recipeName.includes(filter)) {
+                recipe.style.display = '';
+            } else {
+                recipe.style.display = 'none';
+            }
+        });
     });
 });
